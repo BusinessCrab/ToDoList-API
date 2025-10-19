241c40da777eaca64cd4a0f682df3b9e3eb2485e
Nikita Liubimtsev <zixxxota@gmail.com>
2025-10-20 00:07:13 +0300

    added entities

--- src/main/java/com/business_crab/ToDoList/API/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.business_crab.ToDoList.API.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.business_crab.ToDoList.API.entities.Task;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> { }
